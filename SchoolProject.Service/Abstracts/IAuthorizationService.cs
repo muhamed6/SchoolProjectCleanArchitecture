69bcdc0a24ad4f03a13e6e1919eb877d616f5551
muhamed6 <108940519+muhamed6@users.noreply.github.com>
2025-08-08 21:27:47 +0300

    Add Create Role Endpoint (#9)

---
+++ SchoolProject.Service/Abstracts/IAuthorizationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Service.Abstracts
+{
+    public interface IAuthorizationService
+    {
+        public Task<string> AddRoleAsync(string roleName);
+        public Task<bool> IsRoleExist(string roleName);
+    }
+}
