830a8eb19ebfe3f8bab995a67b0489ca1ac4739b
muhamed6 <108940519+muhamed6@users.noreply.github.com>
2025-08-09 18:13:52 +0300

    Authorization (#11)
    * Add Create Role Endpoint
    
    * Add Role Data Seeding
    
    * Add Role Claim To Token

--- SchoolProject.Data/Helpers/UserClaimModel.cs
+++ SchoolProject.Data/Helpers/UserClaimModel.cs
@@ -12,5 +12,6 @@ namespace SchoolProject.Data.Helpers
         public string UserName { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string PhoneNumber { get; set; } = null!;
+        public string Role { get; set; } = null!;
     }
 }
